5744a5f662741296a704920f230f8d7bac47f62f
PapermasteroneGH <149940953+PapermasteroneGH@users.noreply.github.com>
2024-05-03 21:18:21 +0700

    uploading files

---
+++ Firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASpsRZ3N0TMhdXEdSb946rsR4HJEN1HFk",
+  authDomain: "selfpedia-71788.firebaseapp.com",
+  projectId: "selfpedia-71788",
+  storageBucket: "selfpedia-71788.appspot.com",
+  messagingSenderId: "51247215345",
+  appId: "1:51247215345:web:d1af319bf72e48902caf50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
